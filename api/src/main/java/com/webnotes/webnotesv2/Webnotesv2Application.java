bfaaac100850092387a99cfc034921eea267a546
alexey <work.atereshchenko@gmail.com>
2022-05-09 17:35:37 +0300

    init api

---
+++ api/src/main/java/com/webnotes/webnotesv2/Webnotesv2Application.java
@@ -0,0 +1,13 @@
+package com.webnotes.webnotesv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webnotesv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webnotesv2Application.class, args);
+	}
+
+}
